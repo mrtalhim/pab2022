0964fbf16fd47af851d1c0ba4520a5206d2483d5
Mohammad Talham <mrtalhim@gmail.com>
2022-07-03 18:10:32 +0700

    first draft

--- final_project/lib/main.dart
+++ final_project/lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:sklite/naivebayes/naive_bayes.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:sklite/utils/io.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'dart:convert';
 
 void main() {
   runApp(const MyApp());
@@ -48,19 +54,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -75,41 +68,78 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body:
+          PPKMForm(), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class PPKMForm extends StatefulWidget {
+  const PPKMForm({Key? key}) : super(key: key);
+
+  @override
+  _PPKMFormState createState() => _PPKMFormState();
+}
+
+class _PPKMFormState extends State<PPKMForm> {
+  late GaussianNB gnb;
+  Map<String, double> input = {};
+  List inputVar = [
+    'Kasus Konfirmasi per 100.000 penduduk/ minggu',
+    'Rawat Inap RS per 100.000 penduduk/ minggu',
+    'Meninggal per 100.000 penduduk/ minggu',
+    'Positivity Rate (%) (7 hari terakhir)',
+    'Tracing (Rasio Kontak Erat/Kasus Konfirmasi/ Minggu)',
+    'Treatment (BOR/minggu)',
+    'Cakupan Vaksinasi Lengkap (%)',
+    'Cakupan Vaksinasi Lengkap Lansia (%)'
+  ];
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  int output = 0;
+
+  _PPKMFormState() {
+    loadModel("assets/gnb.json").then(
+      (x) => {
+        gnb = GaussianNB.fromMap(json.decode(x)),
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ...List.generate(
+              inputVar.length,
+              (index) => TextFormField(
+                  decoration: InputDecoration(
+                    hintText: inputVar[index],
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'required';
+                    }
+
+                    input.addAll({inputVar[index]: double.parse(value)});
+
+                    return null;
+                  })),
+          ElevatedButton(
+            onPressed: () {
+              print('pressed');
+              _formKey.currentState?.validate();
+
+              output = gnb.predict(List.from(input.values));
+              print(output);
+            },
+            child: Text('Hitung'),
+          ),
+          Text(output.toString())
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
