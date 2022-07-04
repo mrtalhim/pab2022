01eb74026692a2247804af4fce76502335f907c9
Mohammad Talham <mrtalhim@gmail.com>
2022-07-04 11:39:04 +0700

    ui fix

--- final_project/lib/main.dart
+++ final_project/lib/main.dart
@@ -13,39 +13,20 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Prediksi Level Asesmen PPKM',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Prediksi Level Asesmen PPKM'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -56,20 +37,11 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body:
-          PPKMForm(), // This trailing comma makes auto-formatting nicer for build methods.
+      body: PPKMForm(),
     );
   }
 }
@@ -110,7 +82,7 @@ class _PPKMFormState extends State<PPKMForm> {
           List<double>.from(y['class_prior_']),
           List<List<dynamic>>.from(y['sigma_']),
           List<List<dynamic>>.from(y['theta_']),
-          [1, 2, 3],
+          [1, 2, 3], // class
         )
       },
     );
@@ -121,39 +93,50 @@ class _PPKMFormState extends State<PPKMForm> {
   Widget build(BuildContext context) {
     return Form(
       key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ...List.generate(
-              inputVar.length,
-              (index) => TextFormField(
-                  decoration: InputDecoration(
-                    hintText: inputVar[index],
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'required';
-                    }
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            ...List.generate(
+                inputVar.length,
+                (index) => TextFormField(
+                    decoration: InputDecoration(
+                      hintText: inputVar[index],
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'required';
+                      }
 
-                    input[index] = double.parse(value);
+                      input[index] = double.parse(value);
 
-                    return null;
-                  })),
-          ElevatedButton(
-            onPressed: () {
-              _formKey.currentState?.validate();
+                      return null;
+                    })),
+            ElevatedButton(
+              onPressed: () {
+                if (!_formKey.currentState!.validate()) return;
 
-              output = gnb.predict(input);
-              showDialog(
-                context: context,
-                builder: (context) {
-                  return AlertDialog(content: Text(output.toString()));
-                },
-              );
-            },
-            child: Text('Hitung'),
-          ),
-        ],
+                output = gnb.predict(input);
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                        content: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text('PPKM Level'),
+                        Text(output.toString()),
+                      ],
+                    ));
+                  },
+                );
+              },
+              child: Text('Hitung'),
+            ),
+          ],
+        ),
       ),
     );
   }
